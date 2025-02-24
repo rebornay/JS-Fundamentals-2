aa5718508cffa4261bd350ee2e48ef8dde2c8c39
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-02-24 21:30:51 +0100

    Apple Orange

--- script.js
+++ script.js
@@ -17,9 +17,14 @@ function school () {
 }
 
 school ()
-school ()
-school ()
-school ()
-school ()
-school ()
-school ()
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
