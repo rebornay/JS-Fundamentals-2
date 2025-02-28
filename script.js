882dd8b114eb0d996690e9cc477fb25183aa6ab7
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-02-28 22:53:40 +0100

    Deleted

--- script.js
+++ script.js
@@ -102,11 +102,10 @@ const calcAge2 = function (birthYear){
 // const age3 = calcAge3(1991);
 // console.log(age3);
 
-const yearUntilRetirement = birthYear => {
+const yearUntilRetirement = (birthYear) => {
     const age = 2037 - birthYear;
     const retirement = 65 - age;
     return retirement;
 };
 
 console.log(yearUntilRetirement(1991));
-console.log(yearUntilRetirement(19951));
